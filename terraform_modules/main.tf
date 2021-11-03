2eb21bba9031d5add1cb3687ed37ebe7b95ffbdc
Aditya Raj <adyraj02@gmail.com>
2021-11-03 20:59:04 +0530

    Update

--- terraform_modules/main.tf
+++ terraform_modules/main.tf
@@ -7,25 +7,34 @@ data "aws_ecr_repository" "service" {
   name = "docker_image_demo"
 }
 
-# resource "aws_iam_role_policy" "lambda_policy" {
-#   name   = "lambda_policy"
-#   role   = aws_iam_role.lambda_role.id
+# resource "aws_iam_role_policy" "basic_execution_policy" {
+#   name   = "AWS"
+#   role   = aws_iam_role.create_doctor_role.id
 #   policy = jsonencode({
 #     "Version": "2012-10-17",
 #     "Statement": [
 #         {
-#         "Sid": "Stmt1635938747595",
-#         "Action": "logs:*",
-#         "Effect": "Allow",
-#         "Resource": "*"
+#             "Effect": "Allow",
+#             "Action": "logs:CreateLogGroup",
+#             "Resource": "arn:aws:logs:ap-south-1:892418949750:*"
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "logs:CreateLogStream",
+#                 "logs:PutLogEvents"
+#             ],
+#             "Resource": [
+#                 "arn:aws:logs:ap-south-1:892418949750:log-group:/aws/lambda/create_doctor_dev:*"
+#             ]
+#         }
+#       ]
 #     }
-#   ]
-# }
-
 #   )
 # }
-# resource "aws_iam_role" "lambda_role" {
-#   name               = "lambda_role"
+
+# resource "aws_iam_role" "create_doctor_role" {
+#   name               = "createDoctorServiceRoleDev"
 #   assume_role_policy = jsonencode({
 #     "Version": "2012-10-17",
 #     "Statement": [
@@ -43,29 +52,29 @@ data "aws_ecr_repository" "service" {
 # }
 
 
-# module "lambda_function_container_image" {
-#   source = "terraform-aws-modules/lambda/aws"
+module "lambda_function_container_image" {
+  source = "terraform-aws-modules/lambda/aws"
 
-#   function_name = var.functionname
-#   description   = "My awesome lambda function"
-
-#   create_package = false
-
-#   image_uri    = var.imageuri
-#   package_type = "Image"
-# }
-
-resource "aws_lambda_function" "lambda_function_container_image" {
   function_name = var.functionname
   description   = "My awesome lambda function"
-#   role          = aws_iam_role.lambda_role.arn
 
-  image_uri = var.imageuri
+  create_package = false
+
+  image_uri    = var.imageuri
   package_type = "Image"
-
-  environment {
-    variables = {
-      env = "dev"
-    }
-  }
 }
+
+# resource "aws_lambda_function" "lambda_function_container_image" {
+#   function_name = var.functionname
+#   description   = "My awesome lambda function"
+#   role          = aws_iam_role.create_doctor_role.arn
+
+#   image_uri = var.imageuri
+#   package_type = "Image"
+
+#   environment {
+#     variables = {
+#       env = "dev"
+#     }
+#   }
+# }
