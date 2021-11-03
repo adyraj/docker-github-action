73fe942b1c7d9d24201939c94f45c3236058ebce
Aditya Raj <adyraj02@gmail.com>
2021-11-03 20:12:56 +0530

    Update

--- main.tf
+++ main.tf
@@ -17,4 +17,5 @@ module "dockerlambda" {
   source       = "./terraform_modules"
   functionname = "mylambdatest"
   imageuri     = "442803386520.dkr.ecr.ap-south-1.amazonaws.com/docker_image_demo:latest"
+  env = var.env
 }
