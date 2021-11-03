ea88ab9d477f34ba47a13fd04d57ed9c9ae32cd5
Aditya Raj <adyraj02@gmail.com>
2021-11-03 17:57:12 +0530

    Correct IAM

--- terraform_modules/main.tf
+++ terraform_modules/main.tf
@@ -10,11 +10,11 @@ data "aws_ecr_repository" "service" {
 resource "aws_iam_role_policy" "lambda_policy" {
   name   = "lambda_policy"
   role   = aws_iam_role.lambda_role.id
-  policy = file("IAM/lambda_policy.json")
+  policy = "${file("IAM/lambda_policy.json")}"
 }
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_role"
-  assume_role_policy = file("IAM/lambda_assume_role_policy.json")
+  assume_role_policy = "${file("IAM/lambda_assume_role_policy.json")}"
 }
 
 
