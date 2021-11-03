4c3027f1c767086b6e77af0747951d8953b43bf1
Aditya Raj <adyraj02@gmail.com>
2021-11-03 20:42:44 +0530

    Update

--- terraform_modules/main.tf
+++ terraform_modules/main.tf
@@ -7,40 +7,40 @@ data "aws_ecr_repository" "service" {
   name = "docker_image_demo"
 }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name   = "lambda_policy"
-  role   = aws_iam_role.lambda_role.id
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Sid": "Stmt1635938747595",
-        "Action": "logs:*",
-        "Effect": "Allow",
-        "Resource": "*"
-    }
-  ]
-}
+# resource "aws_iam_role_policy" "lambda_policy" {
+#   name   = "lambda_policy"
+#   role   = aws_iam_role.lambda_role.id
+#   policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#         "Sid": "Stmt1635938747595",
+#         "Action": "logs:*",
+#         "Effect": "Allow",
+#         "Resource": "*"
+#     }
+#   ]
+# }
 
-  )
-}
-resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_role"
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-        }
-      ]
-    }
-  )
-}
+#   )
+# }
+# resource "aws_iam_role" "lambda_role" {
+#   name               = "lambda_role"
+#   assume_role_policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#         "Action": "sts:AssumeRole",
+#         "Principal": {
+#             "Service": "lambda.amazonaws.com"
+#         },
+#         "Effect": "Allow",
+#         "Sid": ""
+#         }
+#       ]
+#     }
+#   )
+# }
 
 
 # module "lambda_function_container_image" {
@@ -58,7 +58,7 @@ resource "aws_iam_role" "lambda_role" {
 resource "aws_lambda_function" "lambda_function_container_image" {
   function_name = var.functionname
   description   = "My awesome lambda function"
-  role          = aws_iam_role.lambda_role.arn
+#   role          = aws_iam_role.lambda_role.arn
 
   image_uri = var.imageuri
   package_type = "Image"
