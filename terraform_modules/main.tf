6069fa93f8c2cf979cc939a846e55329c52cbb32
Aditya Raj <adyraj02@gmail.com>
2021-11-03 17:04:16 +0530

    Test IAM

--- terraform_modules/main.tf
+++ terraform_modules/main.tf
@@ -7,15 +7,45 @@ data "aws_ecr_repository" "service" {
   name = "docker_image_demo"
 }
 
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = file("IAM/lambda_policy.json")
+}
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = file("IAM/lambda_assume_role_policy.json")
+}
 
-module "lambda_function_container_image" {
-  source = "terraform-aws-modules/lambda/aws"
 
+# module "lambda_function_container_image" {
+#   source = "terraform-aws-modules/lambda/aws"
+
+#   function_name = var.functionname
+#   description   = "My awesome lambda function"
+
+#   create_package = false
+
+#   image_uri    = var.imageuri
+#   package_type = "Image"
+# }
+
+resource "aws_lambda_function" "lambda_function_container_image" {
   function_name = var.functionname
   description   = "My awesome lambda function"
+  role          = aws_iam_role.lambda_role.arn
 
-  create_package = false
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  #source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  image_uri    = var.imageuri
+  image_uri = var.imageuri
   package_type = "Image"
+
+  environment {
+    variables = {
+      env = "dev"
+    }
+  }
 }
