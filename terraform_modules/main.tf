ec8611f35108d87f1e8af9c60d4fa1a7f576618d
Aditya Raj <adyraj02@gmail.com>
2021-11-03 18:26:34 +0530

    Update

--- terraform_modules/main.tf
+++ terraform_modules/main.tf
@@ -14,7 +14,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
 }
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_role"
-  assume_role_policy = "${file("lambda_assume_role_policy.json")}"
+  assume_role_policy = "${file("/home/runner/work/docker-github-action/docker-github-action/terraform_modules/lambda_assume_role_policy.json")}"
 }
 
 
